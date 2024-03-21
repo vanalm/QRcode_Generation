8a1a2dd6d0aca7743bb615c11f4a37d8e68a1c8b
Jake <jake@cocowireless.org>
2024-03-21 11:03:22 -1000

    made fancier qr codes

--- script.py
+++ script.py
@@ -1,13 +1,39 @@
 import segno
-qr = segno.make('https://youtube.com/shorts/N2IKLQy7ZIY?feature=share')
-qr.save(
-    "scaled_qrcode.png",
-    scale=5,
-    boarder=10, 
-    light="lightblue",
-    dark="darblue",
-    quiet_zone="maroon",
-    data_dark="green",
+qrcode = segno.make('https://youtube.com/shorts/N2IKLQy7ZIY?feature=share')
+# qrcode_rotated = qrcode.to_pil().rotate(45)
 
+## Formatted QR Code
+qrcode_rotated = qrcode.to_pil(
+    scale=5,
+    light="lightblue",
+    dark="green",
+).rotate(45, expand=True)
+qrcode_rotated.save("formatted_rotated_qrcode.png")
+
+
+## Basic QR Code
+qrcode.save(
+    "scaled_qrcode.png",
+    scale=10,
+    # boarder=10, 
+    # light="lightblue",
+    # dark="darblue",
+    # quiet_zone="maroon",
+    # data_dark="green",
+    # data_light="lightgreen",
+
+
+)
+
+
+
+from urllib.request import urlopen
+
+slts_qrcode = segno.make_qr("https://www.youtube.com/watch?v=hTWKbfoikeg")
+nirvana_url = urlopen("https://media.giphy.com/media/LpwBqCorPvZC0/giphy.gif")
+slts_qrcode.to_artistic(
+    background=nirvana_url,
+    target="animated_qrcode.gif",
+    scale=5,
 )
 
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+    # boarder=10, 
+    # light="lightblue",
+    # dark="darblue",
+    # quiet_zone="maroon",
+    # data_dark="green",
+    # data_light="lightgreen",
